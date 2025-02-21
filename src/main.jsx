1ae042dded4faf037eff3200f1c66a1ece687b70
Yusril <yusril886@gmail.com>
2025-02-21 23:51:33 +0800

    import tailwind scrollbar

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,16 @@
+
+/* Node Modules */ 
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
+/* Components */ 
 import './index.css'
+
+/* Css Link*/
 import App from './App.jsx'
+
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
