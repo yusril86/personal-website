2b9243038e8f67a17f0fb155cb2f1432988cb884
Yusril <yusril886@gmail.com>
2025-04-11 16:08:17 +0800

    downgrade version tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+import tailwindscrollbar from 'tailwind-scrollbar';
 export default {
-    content: [
-      './index.html',
-      './src/**/*.{js,ts,jsx,tsx}',
-    ],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [tailwindscrollbar],
+}
