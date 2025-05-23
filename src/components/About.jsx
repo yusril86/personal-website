f56386059c1a69210fc4f74f2a009558fd32f6df
Yusril <yusril886@gmail.com>
2025-05-24 04:02:49 +0800

    about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,27 +1,46 @@
 import { number } from "prop-types";
 
 const aboutItems = [
-    {
-        label : 'Project Done',
-        number: 45,
-    },
-    {
-        label : 'Years of Experience',
-        number: 10,
-    },
+{
+label : 'Project Done',
+number: 45,
+},
+{
+label : 'Years of Experience',
+number: 10,
+},
 ]
 
 const About = () => {
 return (
-    <section id="about">
-        <div className="container">
-            <div className="div">
-                <p className="">
-                    
-                </p>
+<section id="about" className="section">
+    <div className="container">
+
+        <div className="bg-zinc-800/50 p-7 rounded-2xl md:p-12">
+            <p className="text-zinc-300 mb-4 md:mb-8 md:text-xl md:max-w-[60ch]">
+                Welcome! I&apos;m Henry, a professional web developer with a knack for crafting visually stunning and
+                highly functional websites. Combining creativity and technical expertise. I transform your vision into
+                digital masterpiece that excels in both appearance and performance.
+            </p>
+
+            <div className="flex flex-wrap items-center  gap-4 md:gap-7 ">
+                {aboutItems.map(({label, number},key) => (
+                    <div key={key}>
+                        <div className="flex items-center md:mb-2 ">
+                            <span className="text-2xl font-semibold md:text-4xl">{number}</span>
+                            <span className="text-sky-400 font-semibold md:text-3xl">+</span>
+                        </div>
+                        <p className="text-sm text-zinc-400">{label}</p>
+                    </div>
+                ))}
+
+                <img src="/images/logo.svg" alt="Logo" className="ml-auto md:w-[400px] md:h-[40px]" width={30} height={30} />
+
             </div>
         </div>
-    </section>
+
+    </div>
+</section>
 );
 }
 export default About;
