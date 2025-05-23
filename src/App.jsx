871829a086cc1a03e5ed81fa0359df61bf70bab7
Yusril <yusril886@gmail.com>
2025-05-24 05:29:11 +0800

    Skill section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
  import Header from "./components/Header";
  import Hero from "./components/Hero";
  import About from "./components/About";
+ import Skill from "./components/Skill";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <main>
         <Hero/>
        <About/>
+       <Skill/>
       </main>
     </>
     
---
+++ src/components/SkillCard.jsx
@@ -0,0 +1,35 @@
+import PropTypes from "prop-types";
+import Skill from "./Skill";
+
+const SkillCard = ({
+    imgSrc,
+    label,
+    desc,
+    classes
+}) => {
+    return (
+       <div className={'flex items-center gap-3 ring-2 ring-inset  ring-zinc-50/10 rounded-2xl p-3 hover:bg-zinc-800 transition-colors group ' + classes}>
+        <figure className="bg-zinc-700/50 rounded-lg overflow-hidden w-12 h-12 p-2 group-hover:bg-zinc-900 transition-colors ">
+            <img 
+            src={imgSrc}
+            alt={label}
+            width={32}
+            height={32}
+            className="" />
+        </figure>
+
+        <div className="">
+            <h3 className="">{label}</h3>
+            <p className="text-zinc-400 text-sm">{desc}</p>
+        </div>
+       </div>
+    ); 
+}
+
+SkillCard.propTypes = {
+    imgSrc: PropTypes.string.isRequired,
+    label: PropTypes.string.isRequired,
+    desc: PropTypes.string.isRequired,
+    classes: PropTypes.string
+}; 
+export default SkillCard;
