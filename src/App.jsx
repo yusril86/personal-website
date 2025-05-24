233c1d87e611f79c507acfe55f0b1b74645f246c
Yusril <yusril886@gmail.com>
2025-05-25 06:31:39 +0800

    section work

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@
  import Hero from "./components/Hero";
  import About from "./components/About";
  import Skill from "./components/Skill";
+ import Work from "./components/Work";
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
         <Hero/>
        <About/>
        <Skill/>
+        <Work/>
       </main>
     </>
     
